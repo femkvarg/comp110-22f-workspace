f4eebfa126105fe9503657d0104256fe9d01e07e
Femke <femkvarg@ad.unc.edu>
2022-08-18 22:13:43 -0400

    Test run of my own file

---
+++ lessons/00_stored_program.py
@@ -0,0 +1,11 @@
+"""An example, simple program."""
+
+print("Hello,world")
+quiz: int = 100     
+
+"""line 4 has no visible output"""
+
+print(quiz)
+print('Goodbye')
+
+"""This is our recipe for our program."""
